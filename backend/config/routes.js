c20cb8c2d2c1d196b8f578bc8f9beec501d2a4d1
Jaden Pham <tjaden.pham@gmail.com>
2019-07-25 19:24:01 -0700

    first commit with backend features

---
+++ backend/config/routes.js
@@ -0,0 +1,20 @@
+var items = require('../controllers/items.js');
+var path = require('path');
+
+module.exports = function(app){
+    app.post("/register", (req,res)=>{
+        items.register(req,res);
+    })
+    app.post("/create", (req, res) =>{
+        items.create(req,res);
+    })
+    app.get('/all', (req, res) =>{
+        items.all(req, res);
+    })
+    app.get('/one/:id', (req, res)=>{
+        items.one(req,res);
+    })
+    app.all("*", (req, res, next) =>{
+        res.sendFile(path.resolve("./public/dist/public/index.html"))
+    })
+}
